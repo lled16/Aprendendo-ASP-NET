9d099f0c7f0ae8ee964ba79f20e276f156a15bfa
João <vitoralves1604@gmail.com>
2022-02-09 01:24:33 -0300

    Aprendendo .NET

--- obj/Debug/net5.0/web.AssemblyInfo.cs
+++ obj/Debug/net5.0/web.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("WEB")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("WEB")]
-[assembly: System.Reflection.AssemblyTitleAttribute("WEB")]
+[assembly: System.Reflection.AssemblyProductAttribute("web")]
+[assembly: System.Reflection.AssemblyTitleAttribute("web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
